08bdf5c83c67e0e7da2212004f75a3ef12172cc6
Jay Wes <wesleyjay202@gmail.com>
2024-06-19 07:03:48 -0700

    Mobile Menu

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,35 +1,46 @@
-const input = document.getElementById('link-input');
-const linkForm = document.getElementById('link-form');
-const errMSg = document.getElementById('err-msg');
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
 
-linkForm.addEventListener('submit', formSubmit);
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+btn.addEventListener('click', navToggle)
+linkForm.addEventListener('submit', formSubmit)
+
+// Toggle Mobile Menu
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
 
 // Validate a URL
 function validURL(str) {
     var pattern = new RegExp(
-      '^(https?:\\/\\/)?' + // protocol
+    '^(https?:\\/\\/)?' + // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
         '((\\d{1,3}\\.){3}\\d{1,3}))' +
         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
         '(\\?[;&a-z\\d%_.~+=-]*)?' +
         '(\\#[-a-z\\d_]*)?$',
-        'i'
-    )
-    return !!pattern.test(str)
+    'i'
+)
+return !!pattern.test(str)
 }
 
 function formSubmit(e) {
-    e.preventDefault();
-    
+    e.preventDefault()
+
     if (input.value === '') {
-        errMSg.innerHTML = "Please Enter A Link";
-        input.classList.add('border-red');
+        errMsg.innerHTML = 'Please enter something'
+        input.classList.add('border-red')
     } else if (!validURL(input.value)) {
-        errMSg.innerHTML = 'Please Enter a Valid URL'
+        errMsg.innerHTML = 'Please enter a valid URL'
         input.classList.add('border-red')
     } else {
-        errMSg.innerHTML = ''
+        errMsg.innerHTML = ''
         input.classList.remove('border-red')
         alert('Success')
-    };
+    }
 }
